04fefb629c46816f7deb529dc46d3fa77b6c0178
AustrisLukas <austris.lukas@gmail.com>
2026-01-10 12:41:03 +0000

    UI refactor for DocuMind layout and code

--- app/pages/pageA1_doc.py
+++ app/pages/pageA1_doc.py
@@ -6,15 +6,14 @@ from helpers import data_utils
 from helpers import helpers
 
 
-# Page config
+# PAGE CONFIG
 st.set_page_config(
     page_title="DocuMind",
     page_icon="app/assets/images/favicon_v2.png",
     layout="wide",
 )
 
-# Create chat enabled/disabled chat input element based on chat_enabled condition.
-# Chat created outside col1,col2 to ensure it is pinned to the bottom of the view port
+# CREATE A CHAT INPUT ELEMENT AND DISABLE INPUT IF st.chat_enabled=False
 if "chat_enabled" in st.session_state and st.session_state.chat_enabled == True:
     user_input = st.chat_input("Type your question..")
 else:
@@ -23,9 +22,9 @@ else:
         "Upload a document, then type a question..", disabled=True
     )
 
-# Create col1 and col2 to separate page contents
 
-# Side section (if a document is uploaded)
+#   ---***--- SIDE CONTROLS ---***---
+# DISPLAY CHAT CONTROL SECTION IN THE SIDEBAR IF A VALID DOCUMENT IS UPLOADED
 with st.sidebar:
     if "chat_enabled" in st.session_state and st.session_state.chat_enabled == True:
         st.header("References", divider="red")
@@ -40,34 +39,39 @@ with st.sidebar:
         st.button("Clear Chat", use_container_width=True, on_click=helpers.clear_chat)
         st.button("Reset Chat", use_container_width=True, on_click=helpers.reset_chat)
 
-# Main section
-# Check for for presence of uploaded and processed (in bytes) file as 'file_data'.
-# If a document is uploaded, on_chage=helpers.enable_chat is triggered to enable chat input
+
+#   ---***--- MAIN SECTION ---***---
+# CHECK FOR THE PRESENSE OF UPLOADED AND PROCESSED (in bytes) FILE IN session_state AS 'file_data'
+
+# IF file_data IS NOT FOUND, RENDER INITIAL *WELCOME PAGE*
 if "file_data" not in st.session_state:
     st.session_state.chat_history = []
-    st.title("Welcome to DocuMind")
-    st.file_uploader(
-        "Upload your document to activate the chat and start asking questions. DocuMind provides clear, context-aware insights from your content.",
-        type="pdf",
-        key="uploaded_file",
-        on_change=data_utils.process_upload,
+    st.header("📚 Welcome to DocuMind", divider="red")
+    st.subheader(
+        "Upload your document to activate the chat and start asking questions."
     )
-# If user uploaded a file, proceed to process the file
-# else if 'file_data' already exists, render without file upload prompt
+    st.write("DocuMind provides clear, context-aware insights from your content.")
+    col1, col2 = st.columns([6, 4])
+    with col1:
+        st.file_uploader(
+            "",
+            type=["pdf", "docx", "xlsx", "csv"],
+            accept_multiple_files=False,
+            key="uploaded_file",
+            on_change=data_utils.process_upload,
+        )
+
+# ELSE PATHWAY IF st.session_state.file_data IS FOUND.
+# RENDERS CHAT-ENABLED INTERFACE
 else:
     st.header("📚 DocuMind", divider="red")
     st.write(st.session_state.file_name)
     st.badge("Processed", icon=":material/check:", color="green")
-
-    # Populate chat with messages from chat_history
     helpers.load_chat_history()
 
     if user_input:
-        # append user input to the chat history array
-        st.session_state.chat_history.append({"role": "user", "content": user_input})
-        # markdown user input to chat (to ensure there is no delay in message appearing in chat)
-        with st.chat_message("user"):
-            st.markdown(user_input)
+        helpers.send_message("user", user_input)
+
         # generate AI Response
         with st.spinner("thinking..", show_time=True):
             ai_response = openai_service.get_openai_response(
