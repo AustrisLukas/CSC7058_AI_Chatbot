677f70f9d569d7a7470487aa9176784eabdf3ea2
AustrisLukas <austris.lukas@gmail.com>
2026-02-07 11:55:45 +0000

    tested cases for csv extraction

---
+++ tests/helpers/test_extraction_csv.py
@@ -0,0 +1,29 @@
+from app.helpers.data_utils import extract_csv
+from app.helpers.exceptions import DocumentExtractionError
+import pytest
+
+
+# TEST CASE TO VERIFY PDF EXTRACTION RETURNS
+def test_extract_csv_returns_text():
+    with open("tests/fixtures/sample_csv.csv", "rb") as file:
+        text = extract_csv(file)
+
+        assert text != ""
+
+
+# TEST CASE TO VERIFY PDF CONTAINS EXPECTED WORDS
+def test_extract_csv_contains_expected_word():
+    with open("tests/fixtures/sample_csv.csv", "rb") as file:
+        text = extract_csv(file)
+        print(text)
+
+        assert "data1" in text.lower()
+        assert "10" in text.lower()
+        assert "data2" in text.lower()
+
+
+def test_extract_csv_empty():
+    # Use a sample empty or invalid PDF in your fixtures
+    with pytest.raises(DocumentExtractionError):
+        with open("tests/fixtures/empty_csv.csv", "rb") as file:
+            extract_csv(file)
